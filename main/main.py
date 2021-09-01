15aceb309fa8bbbb2d90b0c6612f678e16334269
hanbin8269 <hanbin8269@gmail.com>
2021-09-01 12:02:19 +0900

    feat: add main.py

---
+++ main/main.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@db/main"
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+@dataclass
+class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint("user_id", "product_id", name="user_product_unique")
+
+
+@app.route("/api/products")
+def index():
+    return jsonify(Product.query.all())
+
+
+@app.route("/api/products/<int:id>/like", methods=["POST"])
+def like(id):
+    req = requests.get("http://docker.for.mac.localhost:8000/api/user")
+    return jsonify(req.json())
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
