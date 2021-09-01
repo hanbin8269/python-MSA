2ecc119d30394fa35743f2355e92dd5bf5e96b53
hanbin8269 <hanbin8269@gmail.com>
2021-09-01 12:01:23 +0900

    feat: add publish events process

--- admin/products/views.py
+++ admin/products/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 
 from .models import Product, User
 from .serializers import ProductSerializer
+from .producer import publish
 
 
 class ProductViewSet(viewsets.ViewSet):
@@ -18,6 +19,7 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish("product_created", serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -30,11 +32,14 @@ class ProductViewSet(viewsets.ViewSet):
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish("product_updated", serializer.data)
+
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         product.delete()
+        publish("product_deleted", pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
